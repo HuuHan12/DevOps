6cda3ae37f6c30b9ab59593d3458e467ab53060b
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2026-07-14 14:48:08 +0700

    1: add Dockerfile and docker-compose for dev _ HuuHan

--- FE/src/components/utils/axiosCustomize.js
+++ FE/src/components/utils/axiosCustomize.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-import store from "../../store/index";  
+import store from "../../store/index";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8085/",
+  baseURL: "http://13.250.118.158:8085/",
 });
 
 instance.interceptors.request.use(
   (config) => {
     const state = store.getState();
-    let accessToken = state.user.accessToken; 
+    let accessToken = state.user.accessToken;
 
     if (!accessToken) {
-      accessToken = localStorage.getItem('accessToken');  
+      accessToken = localStorage.getItem('accessToken');
     }
 
     if (accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;  
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
     }
 
     return config;
@@ -25,4 +25,4 @@ instance.interceptors.request.use(
   }
 );
 
- export { instance };
+export { instance };
